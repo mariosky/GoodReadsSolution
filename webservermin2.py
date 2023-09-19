51d5d82541733071022989356bdcbb50e35d3532
Mario Garcia-Valdez <mariosky@gmail.com>
2023-09-19 11:31:41 -0700

    index

--- webservermin2.py
+++ webservermin2.py
@@ -3,7 +3,9 @@ from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
 import re
+import redis
 
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     @cached_property
@@ -25,7 +27,7 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", "text/html")
         self.end_headers()
         response = f"""
-         {books[book_id]}
+         {r.get(book_id).decode()}
     <p>  Ruta: {self.path}            </p>
     <p>  URL: {self.url}              </p>
     <p>  HEADERS: {self.headers}      </p>
@@ -36,12 +38,8 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        response = f"""
-    <h1> Welcome to Great Books </h1>
-    <p>  Ruta: {self.path}            </p>
-    <p>  URL: {self.url}              </p>
-    <p>  HEADERS: {self.headers}      </p>
-"""
+        with open('html/index.html') as f:
+            response = f.read()
         self.wfile.write(response.encode("utf-8"))
 
     def get_method(self, path):
@@ -52,21 +50,9 @@ class WebRequestHandler(BaseHTTPRequestHandler):
 
 
 mapping = [
-            (r'^/Book/(?P<book_id>\d+)$', 'get_book'),
+            (r'^/books/(?P<book_id>\d+)$', 'get_book'),
             (r'^/$', 'get_index')
         ]
-
-books = {
-            '1':"""
-            <h1> Book 1 </h1>
-            """, 
-            '2':"""
-            <h1> Book 2 </h1>
-            """, 
-            '3':"""
-            <h1> Book 3 </h1>
-            """, 
-        }
 
 if __name__ == "__main__":
     print("Server starting...")
